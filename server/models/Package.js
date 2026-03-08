37967313396db5bf00bcce9c4b696a8c535e24b1
Wenard Santos <wenardken57@protonmail.com>
2026-03-08 14:26:57 +0800

    add package schema

---
+++ server/models/Package.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const packageSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  prince: { type: Number, required: true },
+  items: [{ type: String }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Package", packageSchema);
