e135dcc91b60e97c4ab92015550e2ccf70e86d77
Wenard Santos <wenardken57@protonmail.com>
2026-03-08 20:14:10 +0800

    update package routes

--- server/routes/packageRoutes.js
+++ server/routes/packageRoutes.js
@@ -1,39 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const Package = require("../models/Package");
 
-// Create package
-router.post("/", async (req, res) => {
-  const pkg = new Package(req.body);
-  await pkg.save();
-  res.status(201).json(pkg);
-});
+const {
+  createPackage,
+  getPackages,
+  getPackageById,
+  updatePackage,
+  deletePackage,
+} = require("../controllers/packageController");
 
-// Get all packages
-router.get("/", async (req, res) => {
-  const packages = await Package.find();
-  res.json(packages);
-});
-
-// Get single package
-router.get("/:id", async (req, res) => {
-  const pkg = await Package.findById(req.params.id);
-  if (!pkg) return res.status(404).json({ error: "Package not found" });
-  res.json(pkg);
-});
-
-// Update package
-router.put("/:id", async (req, res) => {
-  const pkg = await Package.findByIdAndUpdate(req.params.id, req.body, {
-    returnDocument: "after",
-  });
-  res.json(pkg);
-});
-
-// Delete package
-router.delete("/:id", async (req, res) => {
-  await Package.findByIdAndDelete(req.params.id);
-  res.json({ message: "Package deleted" });
-});
+router.post("/", createPackage);
+router.get("/", getPackages);
+router.get("/:id", getPackageById);
+router.put("/:id", updatePackage);
+router.delete("/:id", deletePackage);
 
 module.exports = router;
