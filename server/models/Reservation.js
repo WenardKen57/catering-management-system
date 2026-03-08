1fe1f49a12773c42ba708d4b44d359dd6f4266ef
Wenard Santos <wenardken57@protonmail.com>
2026-03-08 16:32:54 +0800

    add reservation schema

---
+++ server/models/Reservation.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const reservationSchema = mongoose.Schema({
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  packages: [
+    { type: mongoose.Schema.Types.ObjectId, ref: "Package", required: true },
+  ],
+  eventDate: { type: Date, required: true },
+  totalPrice: { type: Number, required: true },
+
+  status: {
+    type: String,
+    enum: ["reserved", "pendingPayment", "confirmed", "completed", "cancelled"],
+    default: "reserved",
+  },
+
+  notes: { type: String },
+
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Reservation", reservationSchema);
