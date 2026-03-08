cd46e551af0701f223b28997c738c4caadb71773
Wenard Santos <wenardken57@protonmail.com>
2026-03-08 20:32:33 +0800

    add db config

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,10 +22,4 @@ app.use("/api/events", eventRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/auth", authRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () => console.log("Server running on port 5000"));
