de67648672e8dc320bc99342f814f3181352412a
Wenard Santos <wenardken57@protonmail.com>
2026-03-08 17:47:59 +0800

    add payment schema

---
+++ server/models/Payment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = mongoose.Schema({
+  reservation: {
+    type: mongoose.Types.ObjectId,
+    ref: "Reservation",
+    required: true,
+  },
+  amount: { type: Number, required: true },
+  method: { type: String, enum: ["cash", "card", "online"], required: true },
+  status: { type: String, enum: ["pending", "paid", "failed"] },
+  paidAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.Model("Payment", paymentSchema);
