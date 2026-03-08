29db14a3f3c91938f314b9d423eaa293d6102a45
Wenard Santos <wenardken57@protonmail.com>
2026-03-08 16:48:13 +0800

    add event schema

---
+++ server/models/Event.js
@@ -0,0 +1,33 @@
+/*
+  scheduled – Event is planned, staff and resources assigned.
+  inProgress – Event is actively happening. Staff are on-site.
+  completed – Event finished successfully. Data can be reported.
+  cancelled – Event called off, possibly due to customer cancellation or internal reasons.
+ */
+
+const mongoose = require("mongoose");
+
+const eventSchema = mongoose.Schema({
+  reservation: {
+    type: mongoose.Types.ObjectId,
+    ref: "Reservation",
+    required: true,
+  },
+  eventName: { type: String },
+  eventDate: { type: Date, required: true },
+  location: { type: String, required: true },
+  packages: [{ type: mongoose.Types.ObjectId, ref: "Package", required: true }],
+
+  staffAssigned: [{ type: mongoose.Types.ObjectId, ref: "User" }],
+
+  status: {
+    type: String,
+    enum: ["scheduled", "inProgress", "completed", "cancelled"],
+    default: "scheduled",
+  },
+
+  notes: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.Model("Event", eventSchema);
