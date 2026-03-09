31714932e5b3f597a226381692049c3ab8d11cac
Wenard Santos <wenardken57@protonmail.com>
2026-03-09 10:12:56 +0800

    add role based access for reservation routes

--- server/routes/reservationRoutes.js
+++ server/routes/reservationRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 const {
   createReservation,
   getReservations,
@@ -8,10 +10,34 @@ const {
   deleteReservation,
 } = require("../controllers/reservationController");
 
-router.post("/", createReservation);
-router.get("/", getReservations);
-router.get("/:id", getReservationById);
-router.put("/:id", updateReservation);
-router.delete("/:id", deleteReservation);
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("customer", "admin"),
+  createReservation,
+);
+router.get("/", authMiddleware, roleMiddleware("admin"), getReservations);
+router.get(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("customer", "admin"),
+  getReservationById,
+);
+
+// customer and admin can edit the reservation details for last minute changes
+router.put(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("customer", "admin"),
+  updateReservation,
+);
+
+// Admin can only delete the reservation
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  deleteReservation,
+);
 
 module.exports = router;
