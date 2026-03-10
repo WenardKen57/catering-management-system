206980acbcea5eab1146b5bd41fd900cc8217f2f
Wenard Santos <wenardken57@protonmail.com>
2026-03-10 09:57:22 +0800

    add auth and role middleware to userRoutes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 const {
   createUser,
@@ -9,10 +11,10 @@ const {
   deleteUser,
 } = require("../controllers/userController");
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.post("/", authMiddleware, roleMiddleware("admin"), createUser);
+router.get("/", authMiddleware, roleMiddleware("admin"), getUsers);
+router.get("/:id", authMiddleware, roleMiddleware("admin"), getUserById);
+router.put("/:id", authMiddleware, roleMiddleware("admin"), updateUser);
+router.delete("/:id", authMiddleware, roleMiddleware("admin"), deleteUser);
 
 module.exports = router;
