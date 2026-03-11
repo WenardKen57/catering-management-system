38d6c3a610b1caa4dd9901f3fe5671450d1f2c4c
Wenard Santos <wenardken57@protonmail.com>
2026-03-11 10:01:47 +0800

    add interceptor

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,4 +4,14 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
+// Request Interceptor: Runs before every single request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    // This attaches the token to the header automatically
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
